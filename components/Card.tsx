1ba0b4b4c23cfc7d92d65eb3afda855fd336b0be
isgin01 <isgin01@gmail.com>
2025-12-30 15:28:32 +0500

    Replace link to point to github

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 
+const ASSETS = {
+  rhombuses: "/img/rhombuses.svg",
+};
+
 export default function Card({ content }: { content: React.ReactNode }) {
   return (
-    <div className="box-border aspect-3/5 max-h-11/12 w-11/12 overflow-auto rounded-4xl border-2 border-black bg-amber-100 sm:h-11/12 sm:w-auto">
+    <div className="from-light to-dusk box-border aspect-3/5 max-h-11/12 w-11/12 overflow-auto rounded-4xl bg-linear-to-b sm:h-11/12 sm:w-auto">
       <div className="min-h-10 p-5">
         <CardHeader />
       </div>
-      <div className="min-h-20 p-5 text-justify text-xl wrap-break-word">
+      <div className="flex h-full flex-col items-center p-5 text-justify text-xl wrap-break-word">
         {content}
       </div>
     </div>
@@ -15,8 +19,9 @@ export default function Card({ content }: { content: React.ReactNode }) {
 
 function CardHeader() {
   return (
-    <div className="flex w-full items-center justify-between text-2xl font-black capitalize">
-      <Link href={"/about"}>About</Link>
+    <div className="text-burgundy font-accent flex w-full items-center justify-between text-3xl capitalize">
+      <Link href={"https://github.com/isgin01"}>GitHub</Link>
+      <img src={ASSETS.rhombuses} />
       <Link href={"/posts"}>Posts</Link>
     </div>
   );
